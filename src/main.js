9b08dfd00fe994e1152f72846e012bfaaf6f49b6
Frank Iriarte <jurrish@gmail.com>
2017-07-26 15:28:00 -0700

    basic scss and changed to tux

--- src/main.js
+++ src/main.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import cowsay from 'cowsay-browser';
+import faker from 'faker';
+
+import '../src/style/_main.scss';
 
 class App extends React.Component {
+
+  constructor (props) {
+    super(props);
+    //setup initial state
+    //when the state is changed, the component re-renders
+    this.state = {
+      content: '',
+    }
+//this allows us to use a specific context
+//invoke handleClick with "this" context when React creates an instance of the component. so, the instance will have state and .handleClick functionality for ITS OWN CONTEXT
+//!!!define handleClick to use the correct context
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick () {
+    console.log(this)
+    this.setState({
+      content: cowsay.say({
+        text: faker.lorem.words(10),
+        f: 'tux'
+      }),
+    })
+    console.log(this.state)
+  }
+
   render () {
     return (
       <div>
-        <h1> hayyyy </h1>
+        <h1> Generate Cowsay Lorem </h1>
+        <button onClick={ this.handleClick }> click me! </button>
+        <pre> { this.state.content } </pre>
       </div>
     )
   }
